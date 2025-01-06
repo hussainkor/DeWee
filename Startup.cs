fb35b2c73569f06a715a07b5ebbaabf46de1e062
Bindu Kumari <142012740+SinhaBindu@users.noreply.github.com>
2025-01-06 12:18:56 +0530

    Initials Pushing Project DeWee

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeWee.Startup))]
+namespace DeWee
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
