ec15a966889b2afbbb07ab92a974464230e75c19
kamlesh singh <kamlesh.laksh@gmail.com>
2025-06-10 00:17:28 +0530

    report file

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeWee.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+		public ActionResult DeWeeReports()
+		{
+			return View();
+		}
+	}
+}
---
+++ Scripts/JS/Reports/DeWeeReports.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+    createChart();
+
+});
+function createChart() {
+    Highcharts.chart('container', {
+        chart: {
+            type: 'areaspline'
+        },
+        lang: {
+            locale: 'en-GB'
+        },
+        title: {
+            text: 'Live Data'
+        },
+        accessibility: {
+            announceNewData: {
+                enabled: true,
+                minAnnounceInterval: 15000,
+                announcementFormatter: function (
+                    allSeries,
+                    newSeries,
+                    newPoint
+                ) {
+                    if (newPoint) {
+                        return 'New point added. Value: ' + newPoint.y;
+                    }
+                    return false;
+                }
+            }
+        },
+        plotOptions: {
+            areaspline: {
+                color: '#32CD32',
+                fillColor: {
+                    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                    stops: [
+                        [0, '#32CD32'],
+                        [1, '#32CD3200']
+                    ]
+                },
+                threshold: null,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: null,
+                    fillColor: 'white'
+                }
+            }
+        },
+        data: {
+            csvURL: urlInput.value,
+            enablePolling: pollingCheckbox.checked === true,
+            dataRefreshRate: parseInt(pollingInput.value, 10)
+        }
+    });
+
+    if (pollingInput.value < 1 || !pollingInput.value) {
+        pollingInput.value = 1;
+    }
+}
